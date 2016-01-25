7eaadd5df4d13af277e71e93d87e0c3977e1d2c8
Ronald E Thompson III <re.thompsoniii@gmail.com>
2016-01-25 13:41:29 -0500

    Precincts reporting percent fix

--- parappa/ms_results.py
+++ parappa/ms_results.py
@@ -12,7 +12,7 @@ class MSReporting:
         ms_topline = ms_call.ms_topline
         if ms_topline != None:
             print "Most Microsoft recent results for IA"
-            print "Precincts reporting: ", round((float(ms_topline.get('PrecinctsReporting',0))/ms_topline.get('TotalPrecincts'))*100,2)
+            print "Precincts reporting: ", round((float(ms_topline.get('PrecinctsReporting',0))/ms_topline.get('TotalPrecincts'))*100,2), "%"
 
             for candidate in ms_topline['StateResults']:
                 print candidate['Candidate']['LastName'], ": ", round((candidate.get('WinPercentage',0))*100,2), "% ",int(candidate.get('Result',0))
